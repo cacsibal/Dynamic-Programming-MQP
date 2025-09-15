be75d183684d4268d1d609962af0f0b4dd2106a4
heineman <heineman@wpi.edu>
2025-09-15 18:06:46 -0400

    First build

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Dynamic-Programming-MQP
+Designed to use 'gradle' for automatic testing, and hook into CI/CD using GitHub actions.
+
+
+
---
+++ app/src/main/java/org/dp/linear/longest_increasing_subsequence/Solution.java
@@ -0,0 +1,33 @@
+package org.dp.linear.longest_increasing_subsequence;
+
+public class Solution {
+    public int compute (int[] nums) {
+        /**
+         * Initialization
+         */
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < dp.length; i++) {
+          dp[i] = 1;
+        }
+
+        int max=0;
+
+        /**
+         * Iterative solution
+         */
+        for (int i=0; i < dp.length; i++) {
+            for (int j=0; j<i; j++){
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+
+            max = Math.max(max,dp[i]);
+        }
+
+        /**
+         * Return maximum longest increasing subsequence.
+         */
+        return max;
+    }
+}
