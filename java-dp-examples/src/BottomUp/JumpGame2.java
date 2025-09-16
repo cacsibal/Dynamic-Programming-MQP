cdac2cc382213414910d7ea1036f740ea65200bf
Matthew Samuels <mlsamuels@wpi.edu>
2025-09-16 10:12:39 -0400

    BottomUp Files

---
+++ java-dp-examples/src/BottomUp/JumpGame2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+        if(nums.length<=1)
+            return 0;
+
+        int lastDist=0;
+        int dist = 0;
+        for(int i = 1;true;i++){
+            int newdist=dist;
+            for(int j = lastDist;j<=dist;j++){
+                if(j+nums[j]>=nums.length-1)
+                    return i;
+                newdist = Math.max(newdist, j+nums[j]);
+            }
+            lastDist=dist;
+            dist=newdist;
+        }
+    }
+}
---
+++ java-dp-examples/src/BottomUp/MaxSubarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int c=nums[0];
+        int m=c;
+        for(int i=1;i<nums.length;i++){
+            if(c<0){
+                c=0;
+            }
+            c+=nums[i];
+            m=Math.max(m,c);
+        }
+        return m;
+    }
+}
