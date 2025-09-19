7aa8350c55f3477554207ef29c1246970c4d367d
cacsibal <cacsibal@gmail.com>
2025-09-19 10:12:22 -0400

    implemented minimum path sum with custom helper functions

---
+++ app/src/main/java/org/dp/MinPathSum.java
@@ -0,0 +1,27 @@
+package org.dp;
+
+import org.utils.*;
+
+public class MinPathSum {
+    public int minPathSum(int[][] grid) {
+        // initialization
+        int r = grid.length;
+        int c = grid[0].length;
+
+        // maximization
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                if(i > 0 && j > 0) {
+                    grid[i][j] = MathUtils.min(grid[i - 1][j], grid[i][j - 1]);
+                } else if(i > 0) {
+                    grid[i][0] += grid[i - 1][0];
+                } else if(j > 0) {
+                    grid[0][j] += grid[0][j - 1];
+                }
+            }
+        }
+
+        // return
+        return grid[r - 1][c - 1];
+    }
+}
---
+++ app/src/main/java/org/utils/MathUtils.java
@@ -0,0 +1,7 @@
+package org.utils;
+
+public class MathUtils {
+    public static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+}
