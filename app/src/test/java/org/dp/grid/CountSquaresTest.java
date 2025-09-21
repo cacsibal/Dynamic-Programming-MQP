2ecd2b0c19578ae98a4337647948c74bd95e8dc2
cacsibal <cacsibal@gmail.com>
2025-09-21 11:46:40 -0400

    added test cases

--- app/src/test/java/org/dp/grid/CountSquaresTest.java
+++ app/src/test/java/org/dp/grid/CountSquaresTest.java
@@ -1,7 +1,30 @@
 package org.dp.grid;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CountSquaresTest {
+    @Test void test1() {
+        CountSquares countSquares = new CountSquares();
 
+        int[][] matrix = new int[][] {
+                new int[] {0, 1, 1, 1},
+                new int[] {1, 1, 1, 1},
+                new int[] {0, 1, 1, 1}
+        };
+
+        assertEquals(15, countSquares.countSquares(matrix));
+    }
+
+    @Test void test2() {
+        CountSquares countSquares = new CountSquares();
+
+        int[][] matrix = new int[][] {
+                new int[] {1, 0, 1},
+                new int[] {1, 1, 0},
+                new int[] {1, 1, 0}
+        };
+
+        assertEquals(7, countSquares.countSquares(matrix));
+    }
 }
