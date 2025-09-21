311e8a539a192e71eb7f53c1672ff8761a24cd60
cacsibal <cacsibal@gmail.com>
2025-09-21 11:46:28 -0400

    fixed impl

--- app/src/main/java/org/dp/grid/CountSquares.java
+++ app/src/main/java/org/dp/grid/CountSquares.java
@@ -9,7 +9,7 @@ public class CountSquares {
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid[0].length; c++) {
                 if (grid[r][c] == 1 && r > 0 && c > 0)
-                    grid[r][c] += MathUtils.min(grid[r - 1][c - 1], MathUtils.min(grid[r - 1][c], grid[r - 1][c - 1]));
+                    grid[r][c] += MathUtils.min(grid[r - 1][c - 1], MathUtils.min(grid[r - 1][c], grid[r][c - 1]));
             }
         }
 
