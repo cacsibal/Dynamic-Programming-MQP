2b78c4de3e9d3c3d5b30f331d4851f9944768d57
cacsibal <cacsibal@gmail.com>
2025-09-23 11:01:56 -0400

    refactored classes

--- app/src/main/java/org/dp/oneSequence/eggDrop.java
+++ app/src/main/java/org/dp/oneSequence/eggDrop.java
@@ -3,27 +3,30 @@ package org.dp.oneSequence;
 public class eggDrop {
     // Function to find minimum number of attempts
     // needed in order to find the critical floor
-    public int eggDropSol(int n, int k) {
+    public int solution(int n, int k) {
 
-        // if there is less than or equal to one floor
+        /**
+         * Initialization
+         */
         if (k == 1 || k == 0)
             return k;
 
-        // if there is only one egg
         if (n == 1)
             return k;
 
-        // to store the minimum number of attempts
         int res = Integer.MAX_VALUE;
 
-        // Consider all droppings from
-        // 1st floor to kth floor
+        /**
+         * Iterative solution
+         */
         for (int i = 1; i <= k; i++) {
-            int cur = 1 + Math.max(eggDropSol(n - 1, i - 1), eggDropSol(n, k - i));
+            int cur = 1 + Math.max(solution(n - 1, i - 1), solution(n, k - i));
             if (cur < res)
                 res = cur;
         }
-
+        /**
+         * return the minimum number of attempts
+         */
         return res;
     }
 }
--- app/src/main/java/org/dp/topDown/HouseRobber.java
+++ app/src/main/java/org/dp/topDown/HouseRobber.java
@@ -4,34 +4,38 @@ import java.util.Arrays;
 
 public class HouseRobber {
 
-    public int rob(int[] nums) {
+    public int solution(int[] nums) {
+        /**
+         * Initialization
+         */
         int n = nums.length;
-        // dp[i] = max money from house i onwards, -1 means uncomputed
         int[] dp = new int[n];
         Arrays.fill(dp, -1);
 
         return helper(nums, 0, dp);
     }
 
+    /**
+     * iterative solution
+     */
     private int helper(int[] nums, int i, int[] dp) {
         // Base case: no more houses
         if (i >= nums.length) {
             return 0;
         }
 
-        // Already computed
         if (dp[i] != -1) {
             return dp[i];
         }
 
-        // Option 1: Rob current house and skip next
         int robCurrent = nums[i] + helper(nums, i + 2, dp);
 
-        // Option 2: Skip current house
         int skipCurrent = helper(nums, i + 1, dp);
 
-        // Store best result
         dp[i] = Math.max(robCurrent, skipCurrent);
+        /**
+         * return maximum amount of money robbed
+         */
         return dp[i];
     }
 }
