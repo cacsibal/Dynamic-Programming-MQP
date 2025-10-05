2fb3b74e91396d5deb62a2f109bd7cb3384f27fb
Matthew Samuels <mlsamuels@wpi.edu>
2025-10-04 23:47:20 -0400

    Added sources to test cases

--- app/src/test/java/org/dp/oneSequence/EggDropTest.java
+++ app/src/test/java/org/dp/oneSequence/EggDropTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class eggDropTest {
+class EggDropTest {
     @Test
     void eggDrop() {
         eggDrop testeggDrop = new eggDrop();
