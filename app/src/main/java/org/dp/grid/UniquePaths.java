585d0bd498230fdf747f1d90df236e296d83166d
cacsibal <cacsibal@gmail.com>
2025-10-27 15:49:15 -0400

    implementated and tested UniquePaths

---
+++ app/src/main/java/org/dp/grid/UniquePaths.java
@@ -0,0 +1,38 @@
+package org.dp.grid;
+
+/**
+ * Unique Paths: How many unique paths are there from top left to bottom right of an m x n grid?
+ * <a href="https://leetcode.com/problems/unique-paths/">...</a>
+ */
+public class UniquePaths {
+    public int solution(int m, int n) {
+        /**
+         * initialization
+         */
+        int[][] dp = new int[m][n];
+
+        /**
+         * optimization
+         */
+        for(int r = 0; r < m; r++) {
+            dp[r][0] = 1;
+        }
+        for(int c = 0; c < n; c++) {
+            dp[0][c] = 1;
+        }
+
+        /**
+         * maximization through iteration
+         */
+        for(int r = 1; r < m; r++) {
+            for(int c = 1; c < n; c++) {
+                dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
+            }
+        }
+
+        /**
+         * return the last element
+         */
+        return dp[m - 1][n - 1];
+    }
+}
