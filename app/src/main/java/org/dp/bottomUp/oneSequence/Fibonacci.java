d645442992e5b774648beef46ae4a96d8b031b3a
cacsibal <cacsibal@gmail.com>
2025-11-07 11:19:59 -0500

    implemented and tested top-down and bottom-up solutions for Fibonacci and Tribonacci sequences

--- app/src/main/java/org/dp/bottomUp/oneSequence/Fibonacci.java
+++ app/src/main/java/org/dp/bottomUp/oneSequence/Fibonacci.java
@@ -1,38 +1,30 @@
 package org.dp.bottomUp.oneSequence;
 
-/**
- * Fibonacci: placeholder description
- *
- * dynprog solution:
- * fibs <- {
- *     F[1] <- 1
- *     F[2] <- 1
- *     F[n] <- F[n - 1] + F[n - 2]
- * }
- */
 public class Fibonacci {
     public int solution(int n) {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
+        /**
+         * Base Cases
+         */
+        if (n == 0) return 0;
+        if (n == 1) return 1;
 
         /**
          * Initialization
          */
-        int[] dp = new int[n];
-        dp[0] = 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
         dp[1] = 1;
 
         /**
-         * Iterative solution
+         * Optimization through Iteration
          */
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
 
         /**
          * Return the last element
          */
-        return dp[n - 1];
+        return dp[n];
     }
 }
-
--- app/src/main/java/org/dp/bottomUp/oneSequence/Tribonacci.java
+++ app/src/main/java/org/dp/bottomUp/oneSequence/Tribonacci.java
@@ -2,19 +2,31 @@ package org.dp.bottomUp.oneSequence;
 
 public class Tribonacci {
     public int solution(int n) {
-        if(n == 0) return 0;
-        if(n == 1 || n == 2) return 1;
+        /**
+         * Base cases
+         */
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
 
-        int[] dp = new int[n];
+        /**
+         * Initialization
+         */
+        int[] dp = new int[n + 1];
 
         dp[0] = 0;
         dp[1] = 1;
         dp[2] = 1;
 
-        for (int i = 3; i < n; i++) {
+        /**
+         * Optimization through Iteration
+         */
+        for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
 
-        return dp[n - 1];
+        /**
+         * Return the last element
+         */
+        return dp[n];
     }
 }
