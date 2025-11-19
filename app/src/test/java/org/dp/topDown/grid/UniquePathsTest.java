9ff8de1defdcf203fd419e2b6878ba1035fb5384
cacsibal <cacsibal@gmail.com>
2025-11-19 16:35:45 -0500

    fixed uniquePaths and test

--- app/src/test/java/org/dp/topDown/grid/UniquePathsTest.java
+++ app/src/test/java/org/dp/topDown/grid/UniquePathsTest.java
@@ -1,8 +1,6 @@
 package org.dp.topDown.grid;
 
-import org.dp.bottomUp.grid.UniquePaths;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UniquePathsTest {
@@ -10,7 +8,7 @@ public class UniquePathsTest {
      * https://leetcode.com/problems/unique-paths/description/
      */
     @Test void test1() {
-        org.dp.bottomUp.grid.UniquePaths testUniquePaths = new org.dp.bottomUp.grid.UniquePaths();
+        UniquePaths testUniquePaths = new UniquePaths();
         assertEquals(3, testUniquePaths.solution(3, 2));
     }
 
@@ -18,7 +16,7 @@ public class UniquePathsTest {
      * https://leetcode.com/problems/unique-paths/description/
      */
     @Test void test2() {
-        org.dp.bottomUp.grid.UniquePaths testUniquePaths = new UniquePaths();
+        UniquePaths testUniquePaths = new UniquePaths();
         assertEquals(28, testUniquePaths.solution(7, 3));
     }
 }
