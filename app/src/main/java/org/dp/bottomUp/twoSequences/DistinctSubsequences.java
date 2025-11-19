d23c5eafeb385b2d49c00b5bb91a5b4525a84a87
cacsibal <cacsibal@gmail.com>
2025-11-19 16:17:55 -0500

    added comments and defined base cases

--- app/src/main/java/org/dp/bottomUp/twoSequences/DistinctSubsequences.java
+++ app/src/main/java/org/dp/bottomUp/twoSequences/DistinctSubsequences.java
@@ -2,15 +2,24 @@ package org.dp.bottomUp.twoSequences;
 
 public class DistinctSubsequences {
     public int solution(String s1, String s2) {
+        /**
+         * initialization
+         */
         int len1 = s1.length();
         int len2 = s2.length();
 
         int[][] dp = new int[len1 + 1][len2 + 1];
 
-        for(int i = 0; i <= len1; i++) {
-            dp[i][0] = 1;
+        /**
+         * base cases: each substring s1.substring(0, r) is its own subsequence
+         */
+        for(int r = 0; r <= len1; r++) {
+            dp[r][0] = 1;
         }
 
+        /**
+         * optimization through iteration
+         */
         for(int r = 1; r <= len1; r++) {
             for(int c = 1; c <= len2; c++) {
                 if(s1.charAt(r - 1) == s2.charAt(c - 1)) {
@@ -21,6 +30,9 @@ public class DistinctSubsequences {
             }
         }
 
+        /**
+         * return the bottom right element
+         */
         return dp[len1][len2];
     }
 }
