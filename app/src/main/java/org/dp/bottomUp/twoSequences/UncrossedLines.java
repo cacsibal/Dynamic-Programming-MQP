f5ec52465648e3db0fc59dbc592238618904c9b2
cacsibal <cacsibal@gmail.com>
2026-01-23 14:38:12 -0500

    retrieve, constructor, test cases done

--- app/src/main/java/org/dp/bottomUp/twoSequences/UncrossedLines.java
+++ app/src/main/java/org/dp/bottomUp/twoSequences/UncrossedLines.java
@@ -7,18 +7,15 @@ public class UncrossedLines {
     int len2;
     int[][] dp;
 
-    public int solution(int[] nums1, int[] nums2) {
-        /**
-         * Initialization
-         */
+    public UncrossedLines(int[] nums1, int[] nums2) {
         this.nums1 = nums1;
         this.nums2 = nums2;
-
         len1 = nums1.length;
         len2 = nums2.length;
-
         dp = new int[len1 + 1][len2 + 1];
+    }
 
+    public int solution() {
         /**
          * Base Cases: dp[0][...] and dp[...][0] are already 0 by default
          */
@@ -49,7 +46,26 @@ public class UncrossedLines {
         return dp[len1][len2];
     }
 
-    public int retrieve() {
-        return -1;
+    public int[] retrieve() {
+        int numLines = dp[len1][len2];
+        int[] path = new int[numLines];
+
+        int r = len1, c = len2;
+        int index = numLines - 1;
+
+        while (r > 0 && c > 0) {
+            if (nums1[r - 1] == nums2[c - 1]) {
+                path[index] = nums1[r - 1];
+                index--;
+                r--;
+                c--;
+            } else if (dp[r - 1][c] > dp[r][c - 1]) {
+                r--;
+            } else {
+                c--;
+            }
+        }
+
+        return path;
     }
 }
