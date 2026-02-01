89c36f7ad69cd19f50d3f9db4e3da5923a1dd00d
heineman <heineman@wpi.edu>
2026-02-01 12:30:32 -0500

    fixed impl for CountSquares

--- app/src/main/java/org/dp/bottomUp/grid/CountSquares.java
+++ app/src/main/java/org/dp/bottomUp/grid/CountSquares.java
@@ -1,14 +1,30 @@
 package org.dp.bottomUp.grid;
 
 public class CountSquares {
-    public int solution(int[][] grid) {
-        /**
-         * Iterative solution
-         */
+
+    int[][] grid;
+
+    public CountSquares(int[][] grid) {
+        this.grid = grid;
+    }
+
+    public int solution() {
+        return solution_bottomup();
+    }
+
+    public int solution_bottomup() {
+        // dp[i][j] represents the side length of the largest square with bottom-right corner at (i, j)
+        int[][] dp = new int[grid.length][grid[0].length];
+        
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid[0].length; c++) {
-                if (grid[r][c] == 1 && r > 0 && c > 0)
-                    grid[r][c] += Math.min(grid[r - 1][c - 1], Math.min(grid[r - 1][c], grid[r][c - 1]));
+                if (grid[r][c] == 1) {
+                    if (r == 0 || c == 0) {
+                        dp[r][c] = 1; // First row or first column
+                    } else {
+                        dp[r][c] = Math.min(dp[r - 1][c - 1], Math.min(dp[r - 1][c], dp[r][c - 1])) + 1;
+                    }
+                }
             }
         }
 
@@ -18,7 +34,7 @@ public class CountSquares {
         int sum = 0;
         for(int r = 0; r < grid.length; r++) {
             for(int c = 0; c < grid[0].length; c++) {
-                sum += grid[r][c];
+                sum += dp[r][c];
             }
         }
 
