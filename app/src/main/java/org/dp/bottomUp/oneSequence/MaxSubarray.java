de580b77c858e41c6533c34fb8d5cdd099c502e5
Matthew Samuels <mlsamuels@wpi.edu>
2026-02-02 13:04:10 -0500

    Added retrieve to bottom up max subarray

--- app/src/main/java/org/dp/bottomUp/oneSequence/MaxSubarray.java
+++ app/src/main/java/org/dp/bottomUp/oneSequence/MaxSubarray.java
@@ -16,33 +16,66 @@ package org.dp.bottomUp.oneSequence;
  */
 class MaxSubarray {
     int[] nums;
+    int[][] dp;
 
     public MaxSubarray(int[] nums){
         this.nums = nums;
+        dp = new int[nums.length][2];
     }
 
     public int solution() {
 
-        /**
-         * Initialization
-         */
-        int c=nums[0];
-        int m=c;
+
 
         /**
          * Iterative solution
          */
-        for(int i=1;i<nums.length;i++){
-            if(c<0){
-                c=0;
+        for(int i=0;i<nums.length;i++){
+            if(i<1){
+                dp[i][0]=nums[0];
+                dp[i][1]=nums[0];
             }
-            c+=nums[i];
-            m=Math.max(m,c);
+            else{
+                dp[i][0]=Math.max(nums[i], dp[i-1][0]+nums[i]);
+                dp[i][1]=Math.max(dp[i][0], dp[i-1][1]);
+            }
         }
 
         /**
          * Return maximum subarray
          */
-        return m;
+        return dp[nums.length-1][1];
+    }
+
+    public String retrieve(){
+        return findPath(nums.length-1, 1);
+    }
+    public String findPath(int x, int y){
+        if(y==0){
+            if(x==0){
+                return "Take element 0";
+            }
+            else{
+                if(nums[x] > dp[x-1][0]+nums[x-1]){
+                    return "Take element " + x;
+                }
+                else{
+                    return findPath(x-1,0)+"\nTake element " +x;
+                }
+            }
+        }
+        else{
+            if(x==0){
+                return findPath(0,1);
+            }
+            else{
+                if(dp[x][0]>dp[x-1][1]){
+                    return findPath(x,0);
+                }
+                else{
+                    return findPath(x-1,1);
+                }
+            }
+        }
     }
 }
