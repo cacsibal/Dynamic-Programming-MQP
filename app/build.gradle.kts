cd7f992e56441183d51e9a286e63cf7c8aece3a1
heineman <heineman@wpi.edu>
2026-02-10 12:17:48 -0500

    added JavaParser as alternate example to CoGen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,9 @@ dependencies {
 
     // This dependency is used by the application.
     implementation(libs.guava)
+
+    // for the JavaParser library    
+    implementation("com.github.javaparser:javaparser-symbol-solver-core:3.28.0")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
